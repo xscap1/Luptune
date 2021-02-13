bb9aa04821ba49bd98f2d06f2fb0953c536c31cb
theo-poujol <theo.poujol@etu.univ-amu.fr>
2021-02-13 18:58:05 +0100

    Changement email

--- luptune/src/components/Contact.vue
+++ luptune/src/components/Contact.vue
@@ -148,7 +148,7 @@
         <svg class="inline-block w-6 h-6 mr-3 text-beige" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
         </svg>
-        <span class="text-white">luptunecontact@gmail.com</span>
+        <span class="text-white">contact@luptune.fr</span>
       </div>
     </div>
     <div class="bg-marine-dark h-10">
